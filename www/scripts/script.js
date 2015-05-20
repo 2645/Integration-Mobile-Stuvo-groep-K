9f34018b2974d440085157631c8c88f661c6a763
2645 <guesse_who_this_is@hotmail.com>
2015-05-20 11:11:46 +0200

    testing ajax

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,8 +1,8 @@
-//$('body').html("TESTING");
+$('body').html("TESTING");
 getEvents();
 
 function getEvents() {
-   //$('body').html("TESTING1"); $.post("http://dtprojecten.ehb.be/~stuvo/public_html/api/agenda.php", function (data) {       
-        //$('body').html("TESTING2");
-    //});
+   $('body').html("TESTING1"); $.post("http://dtprojecten.ehb.be/~stuvo/public_html/api/agenda.php", function (data) {       
+        $('body').html("TESTING2");
+    });
 }
