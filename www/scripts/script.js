a73549be8bf9dcbcd225bed06ab07251dbc86c43
2645 <guesse_who_this_is@hotmail.com>
2015-05-20 11:46:38 +0200

    testing ajax

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -3,6 +3,11 @@ getEvents();
 
 function getEvents() {
     $.post("http://dtprojecten.ehb.be/~stuvo/public_html/api/agenda.php", function (data) {       
-        $('.testingJS').html(data + "DOES THIS SHIT WORK");
+        $('.testingJS').html(generateEventHtml(data));
     });
+}
+
+function generateEventHtml(data){
+    var data = JSON.parse(data);
+    return "testing";
 }
