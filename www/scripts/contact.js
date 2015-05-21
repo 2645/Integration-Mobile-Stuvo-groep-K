388c494f172a1de7c2db80c31464b67e6824e172
2645 <guesse_who_this_is@hotmail.com>
2015-05-21 13:52:15 +0200

    small update

--- www/scripts/contact.js
+++ www/scripts/contact.js
@@ -1,15 +1,24 @@
 getContacts();
 
 function getContacts() {
-    $.post("http://dtprojecten.ehb.be/~stuvo/public_html/api/contact.php", function (data) {
-        $('.events ul').html(generateEventsHtml(data));
+    $.post("http://dtprojecten.ehb.be/~stuvo/public_html/api/contacts.php", function (data) {
+        $('.dienst').html(generateContactsHtml(data));
     });
 }
 
 function generateContactsHtml(data){
     var contacts = JSON.parse(data).contact;
     var htmlString ="";
-    for(var dep in contacts){
-        console.log(contacts[dep]);
+    for(var dienst in contacts){
+        var dienst = contacts[dienst];
+        htmlString +=generateContactHtml(dienst);
     }
+    return htmlString;
+}
+
+function generateContactHtml(dienst){
+    var htmlString ="";
+    var dienst = dienst.departement;
+    var voornaam = dienst.voornaam;
+    var achternaam = dienst.achternaam;
 }
