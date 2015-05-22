1e4060993cc3c69090f0c0a272b7d08079c2f423
GabrielDeschamps <gabriel_deschamps@hotmail.com>
2015-05-22 14:35:37 +0200

    I'm getting there..

--- www/scripts/Diensten.js
+++ www/scripts/Diensten.js
@@ -7,61 +7,58 @@ $(document).ready(function(){
     $('#ToggleContent6').hide();
     
     $('#ToggleContent1 > #emailENpijl > .pijlUp').click(function(){
-        $('#ToggleContent1').hide('fast');
-        $('.DienstTitel1').show('fast');
+        $('#ToggleContent1').slideUp('fast');
+        $('.DienstTitel1').slideDown('fast');
     });
     
     $('.DienstTitel1').click(function(){
-        $('#ToggleContent1').slideDown(function(){
-            $('#ToggleCOntent').show('slow');
-        });
+        $('#ToggleContent1').slideToggle('fast');
     });
     
     $('.DienstTitel2').click(function(){
-        $('#ToggleContent2').toggle('fast');
-        $('.DienstTitel1').show('fast');
-        $('#ToggleContent1').hide('fast');
+        $('#ToggleContent2').slideToggle('fast');
+        $('.DienstTitel1').slideDown('fast');
+        $('#ToggleContent1').slideUp('fast');
     });
 
     $('#ToggleContent2 > #emailENpijl > .pijlUp').click(function(){
-        $('#ToggleContent2').hide('fast');
-        $('.DienstTitel1').show('fast');
+        $('#ToggleContent2').slideUp('fast');
     });
     
     $('.DienstTitel3').click(function(){
-        $('#ToggleContent3').toggle('fast');
-        $('#ToggleContent2').hide('fast');
+        $('#ToggleContent3').slideToggle('fast');
+        $('#ToggleContent2').slideUp('fast');
     });
     
     $('#ToggleContent3 > #emailENpijl > .pijlUp').click(function(){
-        $('#ToggleContent3').hide('fast');
+        $('#ToggleContent3').slideUp('fast');
     });
     
     $('.DienstTitel4').click(function(){
-        $('#ToggleContent4').toggle('fast');
-        $('#ToggleContent3').hide('fast');
+        $('#ToggleContent4').slideToggle('fast');
+        $('#ToggleContent3').slideUp('fast');
     });
     
     $('#ToggleContent4 > #emailENpijl > .pijlUp').click(function(){
-        $('#ToggleContent4').hide('fast');
+        $('#ToggleContent4').slideUp('fast');
     });
     
     $('.DienstTitel5').click(function(){
-        $('#ToggleContent5').toggle('fast');
-        $('#ToggleContent4').hide('fast');
+        $('#ToggleContent5').slideToggle('fast');
+        $('#ToggleContent4').slideUp('fast');
     });
     
     $('#ToggleContent5 > #emailENpijl > .pijlUp').click(function(){
-        $('#ToggleContent5').hide('fast');
+        $('#ToggleContent5').slideUp('fast');
     });
     
     $('.DienstTitel6').click(function(){
-        $('#ToggleContent6').toggle('fast');
-        $('#ToggleContent5').hide('fast');
+        $('#ToggleContent6').slideToggle('fast');
+        $('#ToggleContent5').slideUp('fast');
     });
     
     $('#ToggleContent6 > #emailENpijl > .pijlUp').click(function(){
-        $('#ToggleContent6').hide('fast');
+        $('#ToggleContent6').slideUp('fast');
     });
 });
 
