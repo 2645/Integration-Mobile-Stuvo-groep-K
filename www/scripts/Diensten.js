5d36cec3918ef93c5a4de174f3352d45b8e3c991
2645 <guesse_who_this_is@hotmail.com>
2015-05-23 22:14:40 +0200

    fixed ISSUES, DIENSTEN KLAAR

--- www/scripts/Diensten.js
+++ www/scripts/Diensten.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-
+    $(".dienstContent",$(".dienst").first()).css("display","block");
+    $(".dienst").first().attr("shown","1");
+    $(".dienst .dienstTitel").first().hide();
+    addListener();
 });
+
+function addListener(){
+    $('.dienst').click(function(event){
+        if($(this).attr('shown')!= 1){
+            $('.dienstContent').slideUp("slow");
+            $('.dienst').attr("shown",0);
+            $('.dienstTitel').slideDown();
+            
+            $('.dienstContent',this).slideDown("slow");
+            $(this).attr("shown",1);
+            $(".dienstTitel",this).slideUp();
+        }
+    });
+}
