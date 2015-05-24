e7d1dbf9bddfb254c3eea84d02b742a8fbb7aa34
2645 <guesse_who_this_is@hotmail.com>
2015-05-24 17:51:04 +0200

    imgPijl werkt nu

--- www/scripts/Diensten.js
+++ www/scripts/Diensten.js
@@ -1,20 +1,27 @@
-$(document).ready(function(){
-    $(".dienstContent",$(".dienst").first()).css("display","block");
-    $(".dienst").first().attr("shown","1");
+$(document).ready(function () {
+    $(".dienstContent", $(".dienst").first()).css("display", "block");
+    $(".dienst").first().attr("shown", "1");
     $(".dienst .dienstTitel").first().hide();
     addListener();
 });
 
-function addListener(){
-    $('.dienst').click(function(event){
-        if($(this).attr('shown')!= 1){
+function addListener() {
+    $('.dienstTitel').click(function (event) {
+        if ($(this).parent().attr('shown') != 1) {
+
             $('.dienstContent').slideUp("slow");
-            $('.dienst').attr("shown",0);
-            $('.dienstTitel').slideDown();
-            
-            $('.dienstContent',this).slideDown("slow");
-            $(this).attr("shown",1);
-            $(".dienstTitel",this).slideUp();
+            $('.dienstTitel').slideDown("slow");
+            $('.dienst').attr('shown', 0);
+
+            $(this).slideUp("slow");
+            $('.dienstContent', $(this).parent()).slideDown();
+            $(this).parent().attr('shown', 1);
         }
+    });
+
+    $('.imgPijl').click(function (event) {
+        $(this).parent().slideUp("slow");
+        $('.dienstTitel',$(this).parent().parent()).slideDown("slow");
+        $(this).parent().parent().attr('shown',0);
     });
 }
