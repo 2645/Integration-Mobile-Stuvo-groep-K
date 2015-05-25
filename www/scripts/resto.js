2678d964dc97272b6410d1eda6223df77a8e38e5
cedricbrichau <cedric.brichau@outlook.com>
2015-05-25 15:53:14 +0200

    oke
    mm
    
    nieuws done

--- www/scripts/resto.js
+++ www/scripts/resto.js
@@ -1,5 +1,12 @@
-getMenus(4);
-addActions();
+
+$(document).ready(function () {
+    var campusID = localStorage.getItem("campusID");
+    movePointerToCampus(campusID);
+    getMenus(campusID%3);
+    addActions();
+});
+
+
 
 function addActions() {
     $('.menuWrapperSlider').on("swipeleft", function (event) {
@@ -18,7 +25,9 @@ function addActions() {
         slideLeft($(".menuWrapperSlider"));
     });
     $('.navResto li').click(function (event) {
-        getMenus(0);
+        var width = $(window).width()/6;
+        var id = Math.round($(this).position().left/width);
+        getMenus(id%3);
         var position = $(this).position().left;
         $('#pointer').animate({
             left: position + "px"
@@ -27,10 +36,17 @@ function addActions() {
 
 }
 
+function movePointerToCampus(id){
+    var leftPos = id*$(window).width()/6
+    $('#pointer').animate({
+        left: leftPos + "px"
+    })
+}
+
 function slideLeft(object) {
     var pos = object.position().left;
     var width = $(window).width();
-    if (pos > -width * 4) {
+    if (pos > -width * (($('.menuWrapperSlider .menu').length)-1)) {
         updateDaySlider(-width);
         object.animate({
             left: '-=' + width + 'px'
@@ -58,9 +74,10 @@ function updateDaySlider(distance) {
 }
 
 function getMenus(id) {
-    $.post("http://dtprojecten.ehb.be/~stuvo/public_html/api/resto.php?campus=0", function (data) {
+    $.post("http://dtprojecten.ehb.be/~stuvo/public_html/api/resto.php?campus=" + id, function (data) {
         $(".dagSelectieWrapperSlider").html("");
         $(".menuWrapperSlider").html(generateMenusHtml(data));
+        changeCss();
 
     });
 }
@@ -103,4 +120,10 @@ function generateMenuHtml(dagMenu) {
     htmlString += "</table></div>";
 
     return htmlString;
+}
+
+function changeCss(){
+    $('.menuWrapperSlider').css('width',100 * ($('.menuWrapperSlider .menu').length) + "%")
+    $('.menu').css('width', (100 / ($('.menuWrapperSlider .menu').length) - 6) + "%");
+    console.log(($('.menuWrapperSlider .menu').length))
 }
