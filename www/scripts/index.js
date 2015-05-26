8b9d7faadc6475b84170be73fb3cb3cd111987c2
2645 <guesse_who_this_is@hotmail.com>
2015-05-26 10:10:18 +0200

    index fix

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $("#login").click(function (e) {
-        e.preventDefault();
         window.location.href = 'home.html';
         checkIfValidEmail(em);
     });
