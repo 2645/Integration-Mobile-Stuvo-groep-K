4344a631ab8f0332eb4d59c53d2e5effb573f13b
LievenLuyckx <Lieven@B103-B201-Lieven-Luyckx.local>
2015-05-27 10:47:40 +0200

    test script

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -7,4 +7,10 @@ $(document).ready(function () {
     });
 
     function checkIfValidEmail(em) {}
+
+    $('.inlogForm-input').live('focus', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        window.scrollTo(0, 0); //the second 0 marks the Y scroll pos. Setting this to i.e. 100 will push the screen up by 100px. 
+    });
 });
